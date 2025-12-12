96f62f0c2f76ebb8f2d28b2c19cdd74df65c4afa
Asako Kanno <asa5ng13@gmail.com>
2025-12-12 21:38:18 +0100

    fix bug

--- src/styling/theme.js
+++ src/styling/theme.js
@@ -10,5 +10,10 @@ export const theme = {
         text: "#1a1a1a",
         textSecondary: "#4d4d4d",
         border: "#000000"
+    },
+    breakpoints: {
+        mobile: "(max-width: 480px)",
+        tablet: "(min-width: 481px) and (max-width: 1023px)",
+        desktop: "(min-width: 1024px)"
     }
 }
